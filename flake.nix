598c45eb9ddc25afc2c2c5151eba597e1117f6ef
Ingo Ruhnke <grumbel@gmail.com>
2022-06-23 10:56:24 +0200

    Remove silero left overs from Nix flake

--- flake.nix
+++ flake.nix
@@ -2,12 +2,9 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     flake-utils.url = "github:numtide/flake-utils";
-
-    silero-src.url = "github:snakers4/silero-models?ref=v0.4.1";
-    silero-src.flake = false;
   };
 
-  outputs = { self, nixpkgs, flake-utils, silero-src }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
